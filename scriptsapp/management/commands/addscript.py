6e98cfb3adc68e384fc76679a4f4910655198931
harsha tanguturi <harshatba@gmail.com>
2020-09-08 22:16:16 +0530

    adding django command to create a script in an app

--- scriptsapp/management/commands/addscript.py
+++ scriptsapp/management/commands/addscript.py
@@ -1,9 +1,36 @@
-from django.core.management.base import BaseCommand, CommandError
+import os
+import time
 
+from django.core.management.base import BaseCommand
+
+
+template = """from {} import models
+
+def run():
+    pass
+
+"""
 
 class Command(BaseCommand):
     help = 'Adds a script to write after a migration is applied'
 
-    def handle(self, *args, **options):
-        print(args)
-        self.stdout.write(self.style.SUCCESS('Yayyy "%s"' % args))
+    def add_arguments(self, parser):
+        parser.add_argument('app')
+
+    def handle(self, app, *args, **options):
+        path_to_dir = os.path.dirname(os.path.abspath(__file__)) + '/../../../' + app
+        print(os.listdir(path_to_dir + '/migrations/')[-1])
+        scripts_dir = path_to_dir + '/scripts/'
+        if not os.path.exists(scripts_dir):
+            os.makedirs(scripts_dir)
+        filenames = os.listdir(scripts_dir)
+        # numbered_filenames = list(map(lambda fname: int(fname.split('.')[0]), filenames))
+        # numbered_filenames.sort(reverse=True)
+        script_name = self.get_script_name()
+        f = open(scripts_dir + script_name, 'w+')
+        f.write(template.format(app))
+        f.close()
+        print(scripts_dir + script_name)
+
+    def get_script_name(self):
+        return str(round(time.time())) + '.py'
--- scriptsapp/models.py
+++ scriptsapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class MigrationScripts(models.Model):
+    app = models.CharField(max_length=255)
+    migration_name = models.CharField(max_length=255)
+    applied = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
