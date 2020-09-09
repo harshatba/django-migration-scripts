b20cfb4624c682bcd6d6f8f11f2d1695120c4e61
harsha tanguturi <harshatba@gmail.com>
2020-09-09 16:26:34 +0530

    adding migrations and commands to run the scripts

--- scriptsapp/management/commands/addscript.py
+++ scriptsapp/management/commands/addscript.py
@@ -1,14 +1,10 @@
 import os
 import time
 
-from django.core.management.base import BaseCommand
-
+from django.core.management.base import BaseCommand, CommandError
+from scriptsapp.models import MigrationScripts
 
 template = """from {} import models
-
-def run():
-    pass
-
 """
 
 class Command(BaseCommand):
@@ -18,19 +14,25 @@ class Command(BaseCommand):
         parser.add_argument('app')
 
     def handle(self, app, *args, **options):
-        path_to_dir = os.path.dirname(os.path.abspath(__file__)) + '/../../../' + app
+        path_to_dir = os.getcwd() + '/' + app
+
+        if not os.path.exists(path_to_dir):
+            CommandError('Are you sure app "{}" exists?'.format(app))
+
         print(os.listdir(path_to_dir + '/migrations/')[-1])
         scripts_dir = path_to_dir + '/scripts/'
         if not os.path.exists(scripts_dir):
             os.makedirs(scripts_dir)
         filenames = os.listdir(scripts_dir)
-        # numbered_filenames = list(map(lambda fname: int(fname.split('.')[0]), filenames))
-        # numbered_filenames.sort(reverse=True)
+        numbered_filenames = list(map(lambda fname: int(fname.split('.')[0]), filenames))
+        numbered_filenames.sort(reverse=True)
+        migration_related_to = numbered_filenames[-1]
         script_name = self.get_script_name()
         f = open(scripts_dir + script_name, 'w+')
         f.write(template.format(app))
         f.close()
         print(scripts_dir + script_name)
+        MigrationScripts.objects.create(app=app, name=script_name, migration_name=migration_related_to)
 
     def get_script_name(self):
         return str(round(time.time())) + '.py'
---
+++ scriptsapp/management/commands/runscripts.py
@@ -0,0 +1,25 @@
+import os
+
+from scriptsapp.models import MigrationScripts
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Adds a script to write after a migration is applied'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--app')
+
+    def handle(self, app=None, *args, **options):
+        if 'app' in options and not options['app']:
+            queryset = MigrationScripts.objects.filter(applied=False, app=options['app']).order_by('id')
+        else:
+            queryset = MigrationScripts.objects.filter(applied=False).order_by('id')
+
+        for migration_script in queryset:
+            script_path = os.getcwd() + '/' + migration_script.app + '/scripts/' + migration_script.name
+            print(script_path)
+            open(script_path).read()
+            exec(open(script_path).read())
+            print(migration_script.name + ' .....DONE')
